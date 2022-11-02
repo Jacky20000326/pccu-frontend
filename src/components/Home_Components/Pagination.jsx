ee0935567d6dac99925fd7c8aae964672ca0d3cf
jacky_hong <Jj0932175456@gmail.com>
2022-11-02 14:39:06 +0800

    更改首頁頁次樣式

--- src/components/Home_Components/Pagination.jsx
+++ src/components/Home_Components/Pagination.jsx
@@ -1,35 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { useSelector, useDispatch } from "react-redux"
 import { CHANGE_PAGE_NUMBER } from "../../Store/AnnouncementSlice"
 const Pagination = () => {
+    const [count, setCount] = useState(1)
     // 將useDispatch實體化
     let dispatch = useDispatch()
     // 取得所有資料(資料庫回傳排序需由新到舊)
     let get_Announcement = useSelector(item => item.AnnouncementReducer.value.Search_Result)
     let Pagination_max_Count = Math.round(get_Announcement.length / 3)
-    let Pagination_Count_Array = []
-    // 取得當前頁次
-    let Pagination_count = useSelector((item => item.AnnouncementReducer.value.current_page))
-    for (var i = 1; i <= Pagination_max_Count; i++) {
-        Pagination_Count_Array.push(i)
+    // 下一頁
+    const nextPage = () => {
+        if (count >= Pagination_max_Count) {
+            return
+        }
+        setCount(item => item + 1)
     }
-
-    const Get_Page_Count = (count) => {
-        // dispatch(GET_USER_RESULT())
+    // 上一頁
+    const lastPage = () => {
+        if (count == 1) {
+            return
+        }
+        setCount(item => item - 1)
+    }
+    useEffect(() => {
+        if (count > Pagination_max_Count) {
+            return
+        }
         dispatch(CHANGE_PAGE_NUMBER(count))
-    }
+    }, [count])
+
+
+
     return (
         <PaginationContainer>
-            {Pagination_Count_Array.map(item => {
-                return (
-                    <Pagination_Index activeStyle={Pagination_count === item ? "#F69393" : "#000"
-                    } onClick={() => { Get_Page_Count(item) }}>
-                        {item}
-                    </Pagination_Index>)
-            })
+            <>
+                {
+                    count == 1 ? null : <Pagination_last onClick={() => { lastPage() }}>
+                        上一頁
+                    </Pagination_last>
+                }
+                {
+                    count >= Pagination_max_Count ? null : <Pagination_next onClick={() => { nextPage() }}>
+                        下一頁
+                    </Pagination_next>
+                }
 
-            }
+            </>
+
+
 
         </PaginationContainer >
     )
@@ -38,13 +57,47 @@ const PaginationContainer = styled.div`
     display: flex;
 
 `
-const Pagination_Index = styled.div`
-    margin-right: 1em;
-    color: ${prop => prop.activeStyle};
+
+const Pagination_last = styled.button`
+    margin-right: 20px;
+    color: #F69393;
     transition: 0.3s;
+    width: 70px;
+
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    cursor: pointer;
+    border: none;
+    background: none;
+
+    background-color: #aeaeae;
+    color: #fff;
     &:hover{
-        color:  #F69393;
-        cursor: pointer;
+        background-color: #F69393;
+        color: #fff;
+        
     }
+    
+`
+const Pagination_next = styled.button`
+    margin-right: 1em;
+    color: #F69393;
+    transition: 0.3s;
+    width: 70px;
+
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    cursor: pointer;
+    border: none;
+    background: none;
+    background-color: #aeaeae;
+    color: #fff;
+    &:hover{
+        background-color: #F69393;
+        color: #fff;
+    }
+    
 `
 export default Pagination
